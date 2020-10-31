92d20cdeb7fe5bc877852e44acf837cf5413f9ad
a-jafarzadeh <a.jafarzadeh1998@gmail.com>
2020-10-31 17:47:08 +0000

    init the project

---
+++ Coronavirus_News_Crawler/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Coronavirus_News_Crawler project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Coronavirus_News_Crawler.settings')
+
+application = get_asgi_application()
